736b80cec9dcde93d68ba7c2e62b046087714e1a
ElTinmar <martin.privat@hotmail.fr>
2025-09-15 13:00:55 +0200

    add ffmpeg

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
         "qt_widgets @ git+https://github.com/ElTinmar/qt_widgets.git@main",
         "image_tools @ git+https://github.com/ElTinmar/image_tools.git@main",
         "tqdm",
+        "ffmpeg"
     ],
     extras_require={
         'gpu': ["cupy==12.3.0"]
