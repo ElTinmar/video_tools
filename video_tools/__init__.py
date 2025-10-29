0918f66d759d72d51490791d629308f9aad38009
ElTinmar <martin.privat@hotmail.fr>
2025-10-29 17:09:05 +0100

    clean init

--- video_tools/__init__.py
+++ video_tools/__init__.py
@@ -1,16 +1,3 @@
-# core functionalities
-
-from .background import *
-from .background_widget import *
-from .playlist_widget import *
-from .video_display import *
 from .video_processor import *
 from .video_reader import *
 from .video_writer import *
-
-# optional gpu functionalities
-try:
-    from .background_gpu import *
-except:
-    print('video_tools GPU functionalities disabled')
-
